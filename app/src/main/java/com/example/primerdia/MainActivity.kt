4a98cc17d45c9218a2f11b907bc41da301880d45
JulianValdess <valdesjulian30@gmail.com>
2025-08-26 13:35:45 -0600

    Se agrego un comentario en el Main

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerDiaTheme {
+                //Julian Eloy Valdes Gonzalez
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
